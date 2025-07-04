9033f031d5a24420424aeb9b3bd5ee03e36be39e
zhou2004 <2065594589@qq.com>
2025-07-04 14:41:53 +0800

    local update

---
+++ src/js/myjs.js
@@ -0,0 +1,14 @@
+// myJs.js
+function myFunc1() {
+    console.log("Called myFunc1");
+    musicPlayer.test();
+}
+
+function myFunc2() {
+    console.log("Called myFunc2");
+}
+
+function getTitle() {
+    return "Dynamic Title from JS";
+}
+
